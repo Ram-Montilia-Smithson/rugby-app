2bab992eae1f679fc391f736fdd19347af75a936
Ram Montilia Smithson <ramsmithson1@gmail.com>
2021-05-03 14:42:45 +0300

    making pitch.jsx to look like a pitch

--- src/components/pitch.jsx
+++ src/components/pitch.jsx
@@ -1,16 +1,47 @@
 import React from 'react'
+import "../scss/pitch/pitch.css"
 
 // show the teams line up as circles on a pitch, arrange them based on their position
 // include the subs as well somehow
+// arrange the scss as well
 
 export default function Pitch({team}) {
     return (
         <div className="pitch">
-            <div className="front row">
-                <span>prop</span>
-                <span>hooker</span>
-                <span>prop</span>
+            <div className="horizontal-lines">
+                <div className="in-goal-area-up">
+                    <div className="posts-up">
+                        <div className="crossbar"></div>
+                    </div>
+                </div>
+                <div className="five-yard-up"></div>
+                <div className="tt-up">
+                    {/* <div className="front row"> */}
+                        <span>hrhrh hrhrhrh</span>
+                        <span>2</span>
+                        <span>3</span>
+                    {/* </div> */}
+                </div>
+                <div className="zone-b"></div>
+                <div className="ten-up"></div>
+                <div className="ten-down"></div>
+                <div className="zone-c"></div>
+                <div className="tt-down"></div>
+                <div className="five-yard-down"></div>
+                <div className="in-goal-area-down">
+                    <div className="posts-down">
+                        <div className="crossbar"></div>
+                    </div>
+                </div>
             </div>
+            <div className="vertical-lines">
+                <div className="five-left"></div>
+                <div className="fifteen-left"></div>
+                <div className="mid-pitch"></div>
+                <div className="fifteen-right"></div>
+                <div className="five-right"></div>
+            </div>
+            {/* 
             <div className="second row">
                 <span>lock</span>
                 <span>lock</span>
@@ -34,7 +65,7 @@ export default function Pitch({team}) {
             </div>
             <div className="fullback">
                 <span>fullback</span>
-            </div>
+            </div> */}
         </div>
     )
 }
