e02f2d0156653df39495e53dea122b5c0b7ad3e7
Ram Montilia Smithson <ramsmithson1@gmail.com>
2021-05-03 18:05:41 +0300

    adding players to pitch

--- src/components/pitch.jsx
+++ src/components/pitch.jsx
@@ -3,9 +3,11 @@ import "../scss/pitch/pitch.css"
 
 // show the teams line up as circles on a pitch, arrange them based on their position
 // include the subs as well somehow
-// arrange the scss as well
 
-export default function Pitch({team}) {
+export default function Pitch({ team }) {
+    
+    console.log(team);
+
     return (
         <div className="pitch">
             <div className="horizontal-lines">
@@ -16,17 +18,43 @@ export default function Pitch({team}) {
                 </div>
                 <div className="five-yard-up"></div>
                 <div className="tt-up">
-                    {/* <div className="front row"> */}
-                        <span>hrhrh hrhrhrh</span>
-                        <span>2</span>
-                        <span>3</span>
-                    {/* </div> */}
+                    <span className="player">{team[7].name}</span>
+                    <span className="player">{team[8].name}</span>
+                    <span className="player">{team[9].name}</span>
                 </div>
-                <div className="zone-b"></div>
-                <div className="ten-up"></div>
-                <div className="ten-down"></div>
-                <div className="zone-c"></div>
-                <div className="tt-down"></div>
+                <div className="zone-b">
+                    <div className="second-row">
+                        <span className="player">{team[10].name}</span>
+                        <span className="player">{team[11].name}</span>
+                    </div>
+                    <div className="back row">
+                        <span className="player">{team[12].name}</span>
+                        <span className="player">{team[14].name}</span>
+                        <span className="player">{team[13].name}</span>
+                    </div>
+                </div>
+                <div className="ten-up">
+                    <span className="player">{team[6].name}</span>
+                    <section></section>
+                </div>
+                <div className="ten-down">
+                    <span className="player">{team[5].name}</span>
+                </div>
+                <div className="zone-c">
+                    <div className="centers">
+                        <span className="player">{team[3].name}</span>
+                        <span className="outside-center player">{team[2].name}</span>
+                    </div>
+                </div>
+                <div className="tt-down">
+                    <div className="wings">
+                        <span className="player">{team[4].name}</span>
+                        <span className="player">{team[1].name}</span>
+                    </div>
+                    <div className="fullback">
+                        <span className="player">{team[0].name}</span>
+                    </div>
+                </div>
                 <div className="five-yard-down"></div>
                 <div className="in-goal-area-down">
                     <div className="posts-down">
@@ -37,35 +65,12 @@ export default function Pitch({team}) {
             <div className="vertical-lines">
                 <div className="five-left"></div>
                 <div className="fifteen-left"></div>
-                <div className="mid-pitch"></div>
+                <div className="mid-pitch">
+                    <div></div>
+                </div>
                 <div className="fifteen-right"></div>
                 <div className="five-right"></div>
             </div>
-            {/* 
-            <div className="second row">
-                <span>lock</span>
-                <span>lock</span>
-            </div>
-            <div className="back row">
-                <span>blind side</span>
-                <span>number 8</span>
-                <span>open side</span>
-            </div>
-            <div className="halves">
-                <span>scrum half</span>
-                <span>fly half</span>
-            </div>
-            <div className="centers">
-                <span>inside center</span>
-                <span>outside center</span>
-            </div>
-            <div className="wings">
-                <span>left wing</span>
-                <span>right wing</span>
-            </div>
-            <div className="fullback">
-                <span>fullback</span>
-            </div> */}
         </div>
     )
 }
